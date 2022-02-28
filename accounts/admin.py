721d0f46773955d19fa421f654c3d71d4861e780
sylvester <nkwontasylvester15@gmail.com>
2022-02-28 11:39:05 -0900

    more commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,21 +1,39 @@
 from django.contrib import admin
 from .models import *
-#from .forms omport UserAdminCreationForm, UserAdminChangeForm
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 # Register your models here.
 
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ['email']
-    
-    model = User
- #      form = UserAdminCreationForm
-  #     add_form = UserAdminChangeForm
-    list_display = ['email', 'admin' ]
-    list_filter = [ 'staff', 'active', 'admin']
+User = get_user_model()
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ['email', 'admin']
+    list_filter = ['admin']
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name',)}),
-        ('Permissions', {'fields': ('admin','staff', 'active', )}),
+        (None, {'fields': ('email', 'password',)}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('active', 'staff','admin',)}),
     )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_2')}
+        ),
+    )
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = ()
+
 
 class GuestEmailAdmin(admin.ModelAdmin):
     search_fields = ['email']
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,3 @@
-import email
-from turtle import update
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
