b21291c85fd948413b7462eca690a0641eb4ca0a
sylvester <nkwontasylvester15@gmail.com>
2022-02-27 17:23:08 -0900

    more  commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import *
+#from .forms omport UserAdminCreationForm, UserAdminChangeForm
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    
+    model = User
+ #      form = UserAdminCreationForm
+  #     add_form = UserAdminChangeForm
+    list_display = ['email', 'admin' ]
+    list_filter = [ 'staff', 'active', 'admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('admin','staff', 'active', )}),
+    )
+
+class GuestEmailAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model = GuestEmail
+
+admin.site.register(User, UserAdmin)
+admin.site.register(GuestEmail, GuestEmailAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -8,12 +8,17 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 #3
 class UserManager(BaseUserManager):
     #creating a user
-    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+    def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
 
         if not email:
             raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+        #if not full_name:
+         #   raise ValueError("Users must have a full name")
         user_obj = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
+            full_name=full_name
         )
         user_obj.set_password(password) #can change user password with this pattern.
         user_obj.active = is_active
@@ -22,9 +27,19 @@ class UserManager(BaseUserManager):
         user_obj.save(using=self._db)
         return user_obj
 
-    def create_staffuser(self, email, password=None):
+    def create_staffuser(self, email, full_name=None, password=None):
+        user = self.create_user(
+            email, 
+            full_name=full_name,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, full_name=None, password=None):
         user = self.create_user(
             email,
+            full_name=full_name,
             password=password,
             is_staff=True,
             is_admin=True,
@@ -38,7 +53,7 @@ class UserManager(BaseUserManager):
 #user class- customized. #1
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
-    #full_name = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
@@ -61,6 +76,14 @@ class User(AbstractBaseUser):
 
     def get_short_name(self):
         return self.email
+
+    #5
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    #5
 
 
     @property
